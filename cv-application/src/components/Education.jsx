3db54dcae51c99a470fed9408a8462897331ec86
pp555 <krzhyu01@gmail.com>
2025-03-12 14:34:20 +0800

    general information submission

---
+++ cv-application/src/components/Education.jsx
@@ -0,0 +1,5 @@
+export default function Education() {
+    return (
+        <h2>education</h2>
+    );
+}
---
+++ cv-application/src/components/Practical.jsx
@@ -0,0 +1,5 @@
+export default function Practical() {
+    return (
+        <h2>practical</h2>
+    );
+}
